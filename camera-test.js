7dd851396d1603955c528406660f96a3143a02c5
Franklin Tse <FranklinWhale@users.noreply.github.com>
2020-10-14 15:02:20 +0800

    Update camera-test.js

--- camera-test.js
+++ camera-test.js
@@ -1,8 +1,15 @@
-const webcam = new Webcam(document.getElementById("camera"), "user", document.getElementById("camera-canvas"));
-const photoContainer = document.getElementById("photo-container");
-document.getElementById("take-photo-button").addEventListener("click", () => {
-  const photo = webcam.snap();
-  const img = document.createElement("img");
-  img.setAttribute("src", photo);
-  photoContainer.appendChild(img);
-});
+(async () => {
+  const webcam = new Webcam(document.getElementById("camera"), "user", document.getElementById("camera-canvas"));
+  const photoContainer = document.getElementById("photo-container");
+  await webcam.start();
+  document.getElementById("flip-camera-button").addEventListener("click", async () => {
+    webcam.flip();
+    await webcam.start();
+  });
+  document.getElementById("take-photo-button").addEventListener("click", () => {
+    const photo = webcam.snap();
+    const img = document.createElement("img");
+    img.setAttribute("src", photo);
+    photoContainer.appendChild(img);
+  });
+})();
