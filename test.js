3e55cf2ca63d0a86ba53aebe79f2928f1cb51e75
Franklin Tse <FranklinWhale@users.noreply.github.com>
2020-10-23 18:42:06 +0800

    Fix script errors

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 (async () => {
 	const url = "https://teachablemachine.withgoogle.com/models/4HlANA7_f/";
-    const model = await tmImage.load(`${url}model.json`, `${url}metadata.json`);
-    
-    const testContainerClassList = document.getElementById("test-container").classList;
-    testContainerClassList.remove("loading");
-    testContainerClassList.add("loaded");
+	const model = await tmImage.load(`${url}model.json`, `${url}metadata.json`);
+	
+	const testContainerClassList = document.getElementById("test-container").classList;
+	testContainerClassList.remove("loading");
+	testContainerClassList.add("loaded");
 })();
