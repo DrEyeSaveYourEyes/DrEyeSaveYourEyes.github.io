7c3188e1a1294d1c30bb00d1bd7345b861215959
Franklin Tse <FranklinWhale@users.noreply.github.com>
2020-10-27 12:08:21 +0800

    Simplify logic

--- test.js
+++ test.js
@@ -2,7 +2,5 @@
 	const url = "https://teachablemachine.withgoogle.com/models/4HlANA7_f/";
 	const model = await tmImage.load(`${url}model.json`, `${url}metadata.json`);
 	
-	const testContainerClassList = document.getElementById("test-container").classList;
-	testContainerClassList.remove("loading");
-	testContainerClassList.add("loaded");
+	document.getElementById("test-container").classList.remove("loading");
 })();
