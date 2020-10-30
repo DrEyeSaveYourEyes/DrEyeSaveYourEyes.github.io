2099d467d2001d52738d41ff71a25ed406a7c205
Franklin Tse <FranklinWhale@users.noreply.github.com>
2020-10-30 14:07:15 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 (async () => {
+	"use strict";
+
 	const url = "https://teachablemachine.withgoogle.com/models/4HlANA7_f/";
 	const model = await tmImage.load(`${url}model.json`, `${url}metadata.json`);
 	
