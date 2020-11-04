f3f09ce39742a85053a6bfa32d5d3e793f507972
Franklin Tse <FranklinWhale@users.noreply.github.com>
2020-11-04 16:11:23 +0800

    Change model

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 (async () => {
 	"use strict";
 
-	const url = "model/";
+	const url = "https://teachablemachine.withgoogle.com/models/4HlANA7_f/";
 	const model = await tmImage.load(`${url}model.json`, `${url}metadata.json`);
 	
 	document.getElementById("test-container").classList.remove("loading");
